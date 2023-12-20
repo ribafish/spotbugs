f9a5ea63a573a973875cd9226ef935e9174d2f82
Gasper Kojek <gkojek@gradle.com>
2023-12-20 14:29:58 +0100

    spotlessApply

--- spotbugs-tests/src/test/java/edu/umd/cs/findbugs/workflow/UnionResultsTest.java
+++ spotbugs-tests/src/test/java/edu/umd/cs/findbugs/workflow/UnionResultsTest.java
@@ -10,7 +10,6 @@ import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
 
 class UnionResultsTest {
